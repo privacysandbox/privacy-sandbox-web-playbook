5f3de683cffe317e4de64f952f8167821a25c440
Natalia Markoborodova <nmarkoborodova@google.com>
2025-06-03 11:19:58 +0100

    demo migration

---
+++ public/components.js
@@ -0,0 +1,7 @@
+import '@material/mwc-top-app-bar-fixed';
+import '@material/mwc-button';
+import '@material/mwc-list';
+import '@material/mwc-list/mwc-list-item';
+import '@material/mwc-textfield';
+import '@material/mwc-select';
+import '@material/mwc-snackbar';
